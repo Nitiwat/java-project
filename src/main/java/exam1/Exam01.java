8c8672ea8fde0b09ea27e9b9d56e9492b56881ca
Nitiwat Nasomvas <nitiwat.n@kasikornbank.com>
2017-07-21 15:25:11 +0700

    Exam case2 =>playerA Fifteen

--- src/main/java/exam1/Exam01.java
+++ src/main/java/exam1/Exam01.java
@@ -1,19 +1,25 @@
 package exam1;
 
 public class Exam01 {
+    int ScoreA = 0;
 
     public Exam01(String firstPerson, String secondPerson) {
     }
 
 
     public void playerAWin() {
+        ScoreA+=1;
     }
 
     public void playerBWin() {
     }
 
     public String getScore() {
+        if (ScoreA==1){
+            return "Fifteen-Love";
+        }
         return "Love-All";
+
     }
 
 }
